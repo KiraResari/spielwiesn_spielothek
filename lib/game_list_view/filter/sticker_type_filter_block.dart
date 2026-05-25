a963b796948e08e1f28faea3bb0ff259315b8412
Kira Resari <kira@tri-tail.com>
2026-05-25 14:32:01 +0200

    Made sticker type filter pills into circles

--- lib/game_list_view/filter/sticker_type_filter_block.dart
+++ lib/game_list_view/filter/sticker_type_filter_block.dart
@@ -18,7 +18,7 @@ class StickerTypeFilterBlock extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text('Sticker', style: const TextStyle(fontWeight: FontWeight.w600)),
+        Text('Stickertyp', style: const TextStyle(fontWeight: FontWeight.w600)),
         const SizedBox(height: 10),
         Wrap(
           spacing: 10,
@@ -33,15 +33,27 @@ class StickerTypeFilterBlock extends StatelessWidget {
     return StickerType.filterOrder.map((stickerType) {
       bool isSelected = controller.selectedStickerTypes.contains(stickerType);
       Color textColor = isSelected ? stickerType.textColor : Colors.black;
-      return FilterChip(
-        label: Text(
-          stickerType.name,
-          style: TextStyle(color: textColor),
+      return RawChip(
+        label: SizedBox(
+          width: 70,
+          height: 70,
+          child: Center(
+            child: Text(
+              stickerType.name,
+              style: TextStyle(color: textColor),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
         backgroundColor: stickerType.stickerColor.withAlpha(50),
         selectedColor: stickerType.stickerColor,
-        checkmarkColor: textColor,
+        showCheckmark: false,
         selected: isSelected,
+        shape: CircleBorder(
+          side: isSelected ? const BorderSide() : BorderSide.none,
+        ),
+        padding: EdgeInsets.zero,
+        labelPadding: EdgeInsets.zero,
         onSelected: (_) {
           controller.toggleStickerType(stickerType);
           setState(() {});
